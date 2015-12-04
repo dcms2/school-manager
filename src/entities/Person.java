60b0bcaf7d1f79cbfe5c4e9e204e59e34fe80069
Tomer Simis <tdl.simis@gmail.com>
2015-12-04 16:20:38 -0300

    Updated basic entities

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -1,5 +1,72 @@
 package entities;
 
-public class Person {
+import java.util.Date;
 
+enum Sex{
+	Male, Female;
+}
+
+public class Person {
+	
+	private String name;
+	
+	private Date birthdate;
+	
+	private Sex sex;
+	
+	private Address address;
+	
+	private String email;
+
+	public Person(String name, Date birthdate, Sex sex, Address address, String email) {
+		super();
+		this.name = name;
+		this.birthdate = birthdate;
+		this.sex = sex;
+		this.address = address;
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public Sex getSex() {
+		return sex;
+	}
+
+	public void setSex(Sex sex) {
+		this.sex = sex;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
 }
--- src/entities/Student.java
+++ src/entities/Student.java
@@ -1,5 +1,56 @@
 package entities;
 
-public class Student {
+import java.util.Date;
 
+public class Student extends Person{
+	
+	private int id;
+	
+	private Class classe;
+	
+	private Person father;
+	
+	private Person mother;
+
+	public Student(String name, Date birthdate, Sex sex, Address address, String email, int id, Class classe,
+			Person father, Person mother) {
+		super(name, birthdate, sex, address, email);
+		this.id = id;
+		this.classe = classe;
+		this.father = father;
+		this.mother = mother;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Class getClasse() {
+		return classe;
+	}
+
+	public void setClasse(Class classe) {
+		this.classe = classe;
+	}
+
+	public Person getFather() {
+		return father;
+	}
+
+	public void setFather(Person father) {
+		this.father = father;
+	}
+
+	public Person getMother() {
+		return mother;
+	}
+
+	public void setMother(Person mother) {
+		this.mother = mother;
+	}
+	
 }
