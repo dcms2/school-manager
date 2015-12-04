4a9278c8128afb4677187da9866edeb23f002543
Tomer Simis <tdl.simis@gmail.com>
2015-12-04 16:50:04 -0300

    Created main function

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		boolean running = true;
+		Scanner in = new Scanner(System.in);
+		printWelcome();
+		
+		while(running){
+			printMenu();
+			int opt = in.nextInt();
+			
+			clear();
+		}
+		
+	}
+	
+	public static void printWelcome(){
+		System.out.println("Seja bem vindo ao");
+		System.out.println("   _____      _                 _ __  __                                   ");
+		System.out.println("  / ____|    | |               | |  \\/  |                                  ");
+		System.out.println(" | (___   ___| |__   ___   ___ | | \\  / | __ _ _ __   __ _  __ _  ___ _ __ ");
+		System.out.println("  \\___ \\ / __| '_ \\ / _ \\ / _ \\| | |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ | '__|");
+		System.out.println("  ____) | (__| | | | (_) | (_) | | |  | | (_| | | | | (_| | (_| |  __| |   ");
+		System.out.println(" |_____/ \\___|_| |_|\\___/ \\___/|_|_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|   ");
+		System.out.println("                                                            __/ |          ");
+		System.out.println("                                                           |___/           ");
+	}
+	
+	public static void clear(){
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	}
+	
+	public static void printMenu(){
+		System.out.println("------------- MENU -------------");
+		System.out.println("  1) Cadastrar aluno");
+		System.out.println("  2) Cadastrar professor");
+		System.out.println("\nDigite o número da ação desejada: ");
+	}
+}
